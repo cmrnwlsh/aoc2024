be2deca8a1ff4416b4a23873a2832030cbf97360
cameron <cmrnwlsh@gmail.com>
2024-12-02 22:30:47 -0500

    d2p1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
+#![feature(iter_map_windows)]
+
 use anyhow::Result;
 use std::{
     cmp::Reverse,
     collections::{BinaryHeap, HashMap},
     env,
+    num::ParseIntError,
 };
 
 static EXAMPLE: &str = "
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
 ";
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("{}", d1p2().await?);
+    println!("{}", d2p1().await?);
     Ok(())
 }
 
@@ -54,4 +63,33 @@ async fn d1p2() -> Result<u32> {
         map
     });
     Ok(l.iter().fold(0, |acc, n| acc + n * f.get(n).unwrap_or(&0)))
+}
+
+async fn d2p1() -> Result<u32> {
+    fn cond1(v: &[u32]) -> bool {
+        v.iter().map_windows(|[x, y]| x <= y).all(|b| b)
+    }
+    fn cond2(v: &[u32]) -> bool {
+        v.iter().map_windows(|[x, y]| x >= y).all(|b| b)
+    }
+    fn cond3(v: &[u32]) -> bool {
+        v.iter()
+            .map_windows(|[x, y]| matches!(x.abs_diff(**y), 1..=3))
+            .all(|b| b)
+    }
+    let reports = input(2)
+        .await?
+        .trim()
+        .split('\n')
+        .map(|s| {
+            s.split_whitespace()
+                .map(|l| l.parse().map_err(|e: ParseIntError| e.into()))
+                .collect()
+        })
+        .collect::<Result<Vec<Vec<u32>>>>()?;
+    Ok(reports
+        .iter()
+        .map(|r| ((cond1(r) || cond2(r)) && cond3(r)))
+        .filter(|b| *b)
+        .count() as u32)
 }
